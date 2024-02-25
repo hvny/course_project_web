b3cd2e0f35691d81910ef2392ac8ea8d687704ce
Vadim <elsukov.vadin@gmail.com>
2024-02-25 15:40:32 +0300

    надо скачать шрифты

---
+++ online-store-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./images/logo.svg"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>TESTTT</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
