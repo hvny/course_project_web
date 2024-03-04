ad3f4fd794c94062e40adff72fb16ada675da8a6
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-03-04 16:23:54 +0300

    add: itemPopup

--- online-store-frontend/src/components/ItemCard/ItemCard.js
+++ online-store-frontend/src/components/ItemCard/ItemCard.js
@@ -3,19 +3,35 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
-
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import ItemPopup from '../ItemPopup/ItemPopup';
+import { useState } from 'react';
 import "./ItemCard.css";
 import testImage from "../../images/bulka.jpg"
 
 export default function ItemCard({itemName}) {
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    function handleClick() {
-        console.log("that works");
+    function handleCardClick() {
+        setIsPopupOpen(!isPopupOpen);
     }
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    };
 
     return (
         <Card className="card">
-            <CardActionArea onClick={handleClick}>
+            <CardActionArea onClick={handleCardClick}>
                 <CardMedia
                     component="img"
                     height="140"
@@ -32,6 +48,16 @@ export default function ItemCard({itemName}) {
                     </Typography> */}
                 </CardContent>
             </CardActionArea>
+            <Modal
+                open={isPopupOpen}
+                onClose={handleCardClick}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <ItemPopup />
+                </Box>
+            </Modal>
             <CardActions>
                 <Button size="small" color="primary" className="card__button card__button_add">
                     Добавить
