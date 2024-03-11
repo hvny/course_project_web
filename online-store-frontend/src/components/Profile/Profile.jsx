63eea8eaad448c67a4556b17f1e3f79b5fb49909
Vadim <elsukov.vadin@gmail.com>
2024-03-12 02:18:48 +0300

    fix: надо переопределить глобальные стили у инпута

--- online-store-frontend/src/components/Profile/Profile.jsx
+++ online-store-frontend/src/components/Profile/Profile.jsx
@@ -3,12 +3,31 @@ import "./Profile.css";
 import TextField from '@mui/material/TextField';
 
 export default function Profile() {
+
+    const MyTextField = () => {
+        return (
+          <TextField
+            label="Имя"
+            variant="outlined"
+            inputProps={{
+              style: {
+                '&:focus': {
+                  borderColor: '#ffffff', // цвет границы при фокусе
+                },
+              },
+            }}
+          />
+        );
+      };
     return (
         <section className="profile">
             <h1 className="profile__title title">Профиль</h1>
             <form className="profile__form">
                 <h2 className="profile__subtitle">Ваши данные</h2>
-                <TextField id="outlined-basic" label="Имя" variant="outlined"/>
+                <MyTextField 
+                    // id="outlined-basic" 
+                    // label="Имя" 
+                    />
             </form>
         </section>
     );
--- online-store-frontend/src/index.js
+++ online-store-frontend/src/index.js
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    {/* <StyledEngineProvider injectFirst> */}
+    <StyledEngineProvider injectFirst>
       <App />
-    {/* </StyledEngineProvider> */}
+    </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
