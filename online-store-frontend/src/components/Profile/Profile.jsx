73776aa5cea463999abe102cbf5af12c5b90fdd8
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-03-13 02:55:48 +0300

    add: hook-form, resolver

--- online-store-frontend/src/components/Profile/Profile.jsx
+++ online-store-frontend/src/components/Profile/Profile.jsx
@@ -1,34 +1,30 @@
 import "./Profile.css";
 
 import TextField from '@mui/material/TextField';
+import { useForm } from "react-hook-form";
+import { joiResolver } from "@hookform/resolvers/joi";
 
 export default function Profile() {
+  const schema = [];
+  const {register, hendleSubmit, formState: {errors, isValid, isDirty}} = useForm({
+    resolver:  joiResolver(schema),
+    defaultValues: {
+      firstName: "",
+      phoneNumber: "",
+      email: "",
+    }
+  });
 
-    const MyTextField = () => {
-        return (
-          <TextField
-            label="Имя"
-            variant="outlined"
-            inputProps={{
-              style: {
-                '&:focus': {
-                  borderColor: '#ffffff', // цвет границы при фокусе
-                },
-              },
-            }}
-          />
-        );
-      };
-    return (
-        <section className="profile">
-            <h1 className="profile__title title">Профиль</h1>
-            <form className="profile__form">
-                <h2 className="profile__subtitle">Ваши данные</h2>
-                <MyTextField 
-                    // id="outlined-basic" 
-                    // label="Имя" 
-                    />
-            </form>
-        </section>
-    );
+  return (
+    <section className="profile">
+        <h1 className="profile__title title">Профиль</h1>
+        <form className="profile__form">
+          <h2 className="profile__subtitle">Ваши данные</h2>
+            <TextField 
+                id="outlined-basic" 
+                label="Имя" 
+            />
+        </form>
+    </section>
+  );
 }
