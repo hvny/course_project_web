9554537fd98ff0f5fe171f765aad315c4a603f29
Vadim <elsukov.vadin@gmail.com>
2024-03-16 16:03:47 +0300

    fix: bottom nav

--- online-store-frontend/src/components/CustomBottomNavigation/CustomBottomNavigation.jsx
+++ online-store-frontend/src/components/CustomBottomNavigation/CustomBottomNavigation.jsx
@@ -11,43 +11,48 @@ import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import EventIcon from '@mui/icons-material/Event';
-
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
 
 
 export default function CustomBottomNavigation({currentLocation}) {
     const location = useLocation();
     const [currentTarget, setCurrentTarget] = useState(location.pathname);
-    console.log(currentTarget)
-
-    useEffect(() => {
-        setCurrentTarget(currentLocation);
-    }, [currentTarget]);
-
-    const handleChange = (event, newValue) => {
-        setCurrentTarget(newValue);
-    };
 
     return (
         <div className="bottom-nav">
             <StyledEngineProvider injectFirst>
-                <BottomNavigation sx={{ width: 500 }} value={currentTarget} onChange={(event, newValue) => {setCurrentTarget(newValue)}} className="bottom-nav_content">
+                <BottomNavigation 
+                    sx={{ width: 500 }} 
+                    value={currentTarget} 
+                    onChange={(event, newValue) => {
+                        setCurrentTarget(newValue)
+                    }} 
+                    className="bottom-nav_content"
+                >
                     <BottomNavigationAction
                         label="Меню"
-                        value="/"
+                        value={currentTarget}
                         icon={<FastfoodIcon  className="bottom-nav__icon" />}
                         href="/"
-                        className="bottom-nav__text"
+                        className={`bottom-nav__text `}
                     />
                     <BottomNavigationAction
                         label="Акции"
-                        value="/events"
-                        href="/events"
+                        value={currentTarget}
                         icon={<EventIcon className="bottom-nav__icon" />}
+                        href="/events"
+                        className="bottom-nav__text"
+                    />
+                    <BottomNavigationAction
+                        label="Доставка"
+                        value={currentTarget}
+                        icon={<LocalShippingIcon className="bottom-nav__icon" />}
+                        href="/delivery"
                         className="bottom-nav__text"
                     />
                     <BottomNavigationAction
                         label="Профиль"
-                        value="profile"
+                        value={currentTarget}
                         icon={<PersonOutlineIcon className="bottom-nav__icon" />}
                         href="/profile" 
                         className="bottom-nav__text"
