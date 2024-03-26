c3a75cd3ebaca784ba15b7d26cd76add2923aa78
Vadim Elsukov <elsukov.vadim@gmail.com>
2024-03-26 12:40:53 +0300

    начал офрмлять footer

--- online-store-frontend/src/components/Footer/Footer.jsx
+++ online-store-frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
     return (
         <footer className="footer">
-            
+            <div className="footer__container">
+                <figure className="footer__figure">
+                    <figcaption className="footer__subtitle">Наши социальные сети</figcaption>
+                    <ul className="footer__list">
+                        <li><Link>Вконтакте</Link></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </figure>
+            </div>
         </footer>
     );
 }
